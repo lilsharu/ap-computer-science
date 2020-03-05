c1c7ba82c1576a6699456d8b9ae08170508f498a
Mastermind497 <39387439+Mastermind497@users.noreply.github.com>
2020-03-04 19:17:25 -0500

    Finished BinarySearch

--- src/DataStructures/BinarySearch.java
+++ src/DataStructures/BinarySearch.java
@@ -1,10 +1,20 @@
 package DataStructures;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] integer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(binarySearch(integer, 11));
+    }
     public static boolean binarySearch(int[] arr, int x) {
-        boolean found = false;
-        while (!found) {
+        int min = 0;
+        int max = arr.length - 1;
+        while (min <= max) {
+            int mid = (min + max) / 2;
 
+            if (arr[mid] < x) min = mid + 1;
+            else if (arr[mid] > x) max = mid - 1;
+            else return true;
         }
+        return false;
     }
 }
